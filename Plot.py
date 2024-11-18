fbd5794d1dbc30bb27a072107655ef97f06b5f8f
interstella04 <stellahof@gmail.com>
2024-11-18 13:14:23 +0100

    Plots done, reading started

--- Plot.py
+++ Plot.py
@@ -5,6 +5,11 @@ import matplotlib.cm as cm
 import pandas as pd
 from pathlib import Path
 from Meas import Meas
+from matplotlib import rc
+
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+plt.rcParams.update({'font.size': 13})
 
 class Plot:
     
@@ -17,8 +22,10 @@ class Plot:
             }
         return
         
-    def simple_plot(self, key):
-        
+    def simple_plot(self, key, div_bin = False, box_opt = False):
+
+        fig, ax = plt.subplots()
+
         x = np.zeros(np.size(self.dictio_of_dictios[key]['dBFs']))
         dx = np.zeros(np.size(self.dictio_of_dictios[key]['dBFs']))
         
@@ -26,32 +33,38 @@ class Plot:
             dx[i] = (self.dictio_of_dictios[key]['Bins'][i+1] - self.dictio_of_dictios[key]['Bins'][i])/2
             x[i] = dx[i]+self.dictio_of_dictios[key]['Bins'][i]
         
-        mean = x.mean()
-        dev = x.std()
-        entries = np.size(x)
-        #Textbox settings not the right vakues
-        textstr = '\n'.join((
-            r'Entries: %d' % (entries,),
-            r'Mean: %0.3f' % (mean,),
-            r'Std Dev: %0.4f' % (dev,)))
-        
-        #Font settings (LATEX NOT FOUND)
-        #plt.rcParams.update({
-                #"text.usetex": True,
-                #"font.family": "sans-serif",
-                #"font.sans-serif": "Helvetica"})
-            
-        if key == 'Belle':
-            plt.errorbar(x, self.dictio_of_dictios[key]['dBFs']*10**(-3), self.dictio_of_dictios[key]['dBFErrors']*10**(-3),dx,fmt = 'k.', markersize = 5)
-            plt.ylabel(r'Events $[10^{3}  / 50 \mathrm{MeV}]$', fontsize = 16)
+        # Divide by the width of the bin or not
+        if(div_bin == False):
+            y = self.dictio_of_dictios[key]['dBFs']
+            dy = self.dictio_of_dictios[key]['dBFErrors']
         else:
-            plt.errorbar(x, self.dictio_of_dictios[key]['dBFs'],self.dictio_of_dictios[key]['dBFErrors'],dx, fmt = 'k.', markersize = 5)
-            plt.ylabel(r'$\Delta B \left( B \rightarrow X_S \gamma \right) [10^{-4}$  / 0.1 $\mathrm{GeV}]$', fontsize = 16)
-            
-        plt.xlabel(r'E[GeV]', fontsize = 16, loc = "right")
-        plt.title(r''+ self.dictio_of_dictios[key]["Label"],fontsize = 20)
-        #plt.text(0.05, 0.95, textstr, fontsize = 14, verticalalignment = 'top') not working
-        plt.savefig(''+self.dictio_of_dictios[key]["Label"], dpi = 500)
+            y = self.dictio_of_dictios[key]['dBFs']/dx
+            dy = self.dictio_of_dictios[key]['dBFErrors']/dx
 
-h = Plot()
-h.simple_plot('Belle')
+        #Set Box options
+        if (box_opt):
+            textstr = '\n'.join((
+                '$\\mathrm{Entries:} \\: %d$' % (np.size(x),),
+                '$\\mathrm{Mean:} \\: %0.3f$' % (x.mean(),),
+                '$\\mathrm{Std Dev:} \\: %0.4f$' % (x.std(),)))
+            ax.text(0.8,0.95,textstr,fontsize=12,color="black",bbox=dict(facecolor="white", alpha=1, edgecolor="black"), transform = ax.transAxes)
+        
+        if key == 'Belle':
+            ax.errorbar(x, y*10**(-3), dy*10**(-3),dx,fmt = 'k.', markersize = 5)
+            ax.set_ylabel('$\\mathrm{Events} \\: [10^{3}  / 50 \\mathrm{MeV}]$', fontsize = 16)
+        else:
+            ax.errorbar(x, y,dy,dx, fmt = 'k.', markersize = 5)
+            ax.set_ylabel('$\\Delta B \\left( B \\rightarrow X_S \\gamma \\right) \\: [10^{-4}  / 0.1 \\mathrm{GeV}]$', fontsize = 16)
+              
+        ax.set_xlabel('$E[\\mathrm{GeV}]$', fontsize = 16, loc = "right")
+        ax.set_title('$\\mathrm{%s}$' % self.dictio_of_dictios[key]["Label"],fontsize = 20)
+        plt.savefig('data/'+self.dictio_of_dictios[key]["Label"], dpi = 500)
+
+h1 = Plot()
+h2 = Plot()
+h3 = Plot()
+h4 = Plot()
+h1.simple_plot('Babar_hadtag')
+h2.simple_plot('Belle')
+h3.simple_plot('Babar_sem')
+h4.simple_plot('Babar_incl')
