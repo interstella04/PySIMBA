ae46161ac59afd0ce70f75188777194f01ef80e8
interstella04 <stellahof@gmail.com>
2024-11-19 12:19:17 +0100

    Theoriegegrabbed, aber Dateipfade fehlen

--- Meas.py
+++ Meas.py
@@ -15,40 +15,59 @@ class Meas:
         return data
     
     '''Function to grab theory, currently just for one NNLLNNLO data'''
-    def grab_theory(key):
-        '''end_nll = ['03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
+    def grab_theory(key, mid):
+
+        if mid == 'NNLLNNLO':
+            end_NLL = ['03', '07']
+            '''['03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
                     '035', '045', '055', '065', '075', '085', '095', '0475',
                     '0525', '0625']'''
-        bins = []
-        width = []
-        curr_block = []
-        count = 0
-        #for j in range(np.size(end_nll)):
-        with open('theory/'+ key + '_NNLLNNLO_la07.txt', 'r') as file:
-            for i, line in enumerate(file):
-                if i < 17:  # jumps to line 18, only correct for NNLLNNLO Data
-                    continue
-                line = line.strip()  # removes empty lines
-                if line.startswith("# bin"):  # skips line starting with # bin
-                    line = line.strip('# bin = %d {fEg, }' % (count,))
-                    print(line)
-                    width.append(tuple(map(float, line.split(','))))
-                    count+=1
-                    continue
-                if line == "":
-                    if curr_block:  # if current block not empty
-                        bins.append(np.array(curr_block, dtype=float))
-                        curr_block = []
-                else:
-                    # extract numbers out of line and put them into an array
-                    curr_block.append(list(map(float, line.split())))
-
-        if curr_block:
-            bins.append(np.array(curr_block, dtype=float))
+        else:
+            print('wurde noch nicht implementiert ^^ ')
+            return
         
-        return bins,width
+        collected_data = {}
 
-bins, width = Meas.grab_theory('babar_hadtag')
+        for j, end in enumerate(end_NLL):
+            count = 0
+            curr_block = []
+            bins = []
+            width = []
+            with open('theory/'+ key + '_'+mid+'_la'+end+'.txt', 'r') as file:
+                for i, line in enumerate(file):
+                    if i < 17:  # jumps to line 18, only correct for NNLLNNLO Data
+                        continue
+                    line = line.strip()  # removes empty lines
+                    if line.startswith("# bin"):  # skips line starting with # bin
+                        line = line.strip('# bin = %d {fEgY, }' % (count,))
+                        width.append(list(map(float, line.split(','))))
+                        count+=1
+                        continue
+                    if line == "":
+                        if curr_block:  # if current block not empty
+                            bins.append(np.array(curr_block, dtype=float))
+                            curr_block = []
+                    else:
+                        # extract numbers out of line and put them into an array
+                        curr_block.append(list(map(float, line.split())))
 
+            if curr_block:
+                bins.append(np.array(curr_block, dtype=float))
+        
+            theo_dictio = {
+                "name": 'la'+end,
+                "bins": bins,
+                "width": width
+            }
+
+            collected_data['la'+end] = theo_dictio
+
+        return collected_data
+
+d = Meas.grab_theory('babar_hadtag', 'NNLLNNLO')
+
+print(d['la07']['width'])
+'''
 for i, arr in enumerate(bins):
     print(f"Bin = {i} in range {width[i]}:\n{arr}\n")
+'''
